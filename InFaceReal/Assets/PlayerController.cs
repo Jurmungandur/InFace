a1dc4899e3edf2d548b0f5f801e73d4d56d99460
Jurmungandur <31280587+Jurmungandur@users.noreply.github.com>
2017-12-28 17:25:16 +0100

    Game v1.0

---
+++ InFaceReal/Assets/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private bool shootReady = false;
+
+    private float height = 1;
+    private float width = 1;
+
+    private Vector3 targetPos;
+
+	// Use this for initialization
+	void Start () {
+        targetPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (targetPos.y > transform.position.y)
+        {
+            transform.Translate(new Vector3(0, 0.01f, 0));
+        }
+        if (targetPos.y < transform.position.y)
+        {
+            transform.Translate(new Vector3(0, -0.01f, 0));
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (Vector3.Distance(targetPos,transform.position) < 0.01f)
+            {
+                if (shootReady == false)
+                {
+                    shootReady = true;
+                    targetPos = new Vector3(transform.position.x, transform.position.y + height / 2, transform.position.z);
+                }
+                else
+                {
+                    shootReady = false;
+                    targetPos = new Vector3(transform.position.x, transform.position.y - height / 2, transform.position.z);
+                }
+            }
+        }
+    }
+}
