3e4ea09ef540e65f0de1e7ffc49a6752b5edb4ca
Jurmungandur <31280587+Jurmungandur@users.noreply.github.com>
2017-12-29 13:32:22 +0100

    Game v1.1
    I started inplumenting aiming for the ready to shoot face of the players bahvior.

--- InFaceReal/Assets/PlayerController.cs
+++ InFaceReal/Assets/PlayerController.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     private bool shootReady = false;
+    private bool aiming = false;
 
     private float height = 1;
     private float width = 1;
 
+    Camera viewCamera;
+
     private Vector3 targetPos;
     Rigidbody rb;
 
-	// Use this for initialization
+
 	void Start () {
         targetPos = transform.position;
         rb = GetComponent<Rigidbody>();
+        viewCamera = Camera.main;
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-        if (shootReady == false)
+        Vector3 TheMousePosition = viewCamera.ScreenToWorldPoint(Input.mousePosition); //Get Mouse Position
+        Debug.Log(TheMousePosition);
+
+        if (shootReady == false) //Ready the player to shoot.
         {
             targetPos = transform.position;
         }
@@ -28,11 +35,20 @@ public class PlayerController : MonoBehaviour {
         {
             transform.Translate(new Vector3(0, 0.01f, 0)); 
         }
+
+
+        if (aiming == true) //Aim the player
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                aiming = false;
+            }
+        }
     }
 
     void OnMouseOver()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1)) //Get Input to ready up
         {
                 if (shootReady == false)
                 {
@@ -45,6 +61,18 @@ public class PlayerController : MonoBehaviour {
                     shootReady = false;
                     rb.useGravity = true;
                 }
+        }
+
+
+        if (shootReady == true) //Get Input to start aiming
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (aiming == false)
+                {
+                    aiming = true;
+                }
+            }
         }
     }
 }
